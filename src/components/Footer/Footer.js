8867bc5a7c3b85cec99570a0883130173c9cff7d
Praveen Goswami <praveen828589@gmail.com>
2023-03-13 14:43:31 +0530

    Added Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,35 @@
 import './Footer.css';
 import { wave } from '../../assets';
+import { BsGithub, BsLinkedin, BsInstagram } from 'react-icons/bs';
 
 const Footer = () => {
   return (
     <div className='app__footer'>
         <img src={wave} alt="Footer bg"/>
+        
+        <div className='app__footer-content'>
+          <p className='app__footer-content_copyright'>Copyright &copy; 2023. Praveen Goswami.</p>
+          <div className='app__footer-content_socialIcons'>
+                <a 
+                  href="https://github.com/praveengoswami1996" 
+                  target="_blank"
+                  rel='noreferrer'
+                ><BsGithub className='icon'/></a>
+                
+                <a
+                  href="https://www.linkedin.com/in/praveen-goswami/" 
+                  target="_blank"
+                  rel='noreferrer'
+                ><BsLinkedin className='icon'/></a>
+                
+                <a
+                  href="https://www.instagram.com" 
+                  target="_blank"
+                  rel='noreferrer'
+                ><BsInstagram className='icon'/></a>
+            </div>
+        </div>
+
     </div>
   )
 }
